11f456552c4ede302880f14c15e5a6fd34479747
christoph2806 <christoph2806@users.noreply.github.com>
2025-02-27 13:22:32 +0000

    deploy: 30d7c0dac7dfa18c3013e82433e8a874c100ecba

--- run-jekyll-compose.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Unset problematic Python environment variables
-echo "Unsetting problematic Python environment variables..."
-unset PYTHONHOME PYTHONPATH PYTHONDONTWRITEBYTECODE INSIDE_NEMO_PYTHON
-
-# Run Jekyll with Docker Compose
-echo "Running Jekyll with Docker Compose..."
-docker-compose up 
--- run-jekyll-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Unsetting problematic Python environment variables..."
-unset PYTHONHOME PYTHONPATH PYTHONDONTWRITEBYTECODE INSIDE_NEMO_PYTHON
-
-echo "Running Jekyll locally..."
-bundle exec jekyll serve 
